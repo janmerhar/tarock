cb579611c3bcaafcf270d81ca7a104200abc9c39
Jan Merhar <merharjan@gmail.com>
2022-08-02 02:35:31 +0200

    Add example from fyne.io package into main.go

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }
