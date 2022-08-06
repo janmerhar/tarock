edd33dd3a713ad412863e6fa6688db27bb59fb03
Jan Merhar <merharjan@gmail.com>
2022-08-06 02:03:34 +0200

    Fix bug in deck/deck.go function Shuffle() and add function Draw_n_cards()

--- deck/deck.go
+++ deck/deck.go
@@ -11,7 +11,17 @@ type Deck struct {
 	Cards []cards.Card // current cards in deck of 54
 }
 
-func (deck Deck) Shuffle() {
+func (deck *Deck) Shuffle() {
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(deck.Cards), func(i, j int) { deck.Cards[i], deck.Cards[j] = deck.Cards[j], deck.Cards[i] })
 }
+
+func (deck *Deck) Draw_n_cards(n int) []cards.Card {
+	subslice := deck.Cards[:n]
+	deck.Cards = deck.Cards[n:]
+
+	return subslice
+}
+
+// draw cards from range to range
+// koristno, ko izbiras iz talona
