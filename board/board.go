f4df5b8128846a43047544eeea9ec80b403907e3
Jan Merhar <merharjan@gmail.com>
2022-08-18 22:10:17 +0200

    Create board/board.go file with half made Board type

---
+++ board/board.go
@@ -0,0 +1,19 @@
+package board
+
+import (
+	"github.com/janmerhar/tarock/deck"
+	"github.com/janmerhar/tarock/player"
+	"github.com/janmerhar/tarock/scoreboard"
+)
+
+type Board struct {
+	Deck           deck.Deck       // deck of 54 tarock cards
+	Cards_on_table deck.Deck       // currently placed cards on teble/board
+	Talon          deck.Deck       // cards in talon
+	Players        []player.Player // all the player playing current game
+	Licitators     []player.Player // all the players that play for the points, it is a subset of Board.Players
+	// Licitiotions: trula, kralji, itd.
+	// Klic igre: 1, 2, ..., solo, klop,...
+
+	scoreboard scoreboard.Scoreboard
+}
