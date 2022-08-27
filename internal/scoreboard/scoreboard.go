fa00980050783d4406d6e70153f7898ed2b7d0b7
Jan Merhar <merharjan@gmail.com>
2022-08-27 23:58:57 +0200

    Add scoreboard/scoreboard.go desc

--- internal/scoreboard/scoreboard.go
+++ internal/scoreboard/scoreboard.go
@@ -1,4 +1,21 @@
 package scoreboard
 
 type Scoreboard struct {
+	// map player => score
+	// -> Won't work: use indexes from outside insted
+	// -> making same dimensions as
+	// -> or i can have a copy of players
+
+	// kako naredim radelce
+	// mogoce naredim map player => (score, st_radelcev)
+
+	// mogoce se hranim odigrane igre
+
+	// lahko dam vsako lastnost kot svojo entiteto
+	// ter jo potem z metodami zdruzim
+
+	// mogoce se dodam funkcionalnost shrambe scoreboarda v
+	// text ali pa "pickle" format
 }
+
+// Count_points(deck.)
