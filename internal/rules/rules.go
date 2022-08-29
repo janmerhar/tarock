1e92f9cb653ca9e546cd3a9c8d831620729cab44
Jan Merhar <merharjan@gmail.com>
2022-08-29 21:06:11 +0200

    Add game/game.go file with empty  class

---
+++ internal/rules/rules.go
@@ -0,0 +1,23 @@
+package rules
+
+type Rules struct {
+}
+
+/*
+	Rules for number of players:
+	- 2, 3 and 4
+
+	Rules for throwing cards from hand on table:
+	- throw tarock on color, if player has no color
+	- throw color on tarock/different color, if player has no tarock
+	- ...
+
+	Rules for bidding:
+	- Who is eligable for bidding
+	- What is availible for bidding
+
+	Rules for licitations:
+	- Power ranking for types of bidding
+	- Types of bidding for every game
+
+*/
