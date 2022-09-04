9d5e4b18b23149921c21c464ba87d26a46bd0851
Jan Merhar <merharjan@gmail.com>
2022-09-04 22:36:12 +0200

    Add DrawEntireDeckTop() in gui/gui_functions.go

--- internal/gui/gui_functions.go
+++ internal/gui/gui_functions.go
@@ -1,1 +1,14 @@
 package gui
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+func DrawEntireDeckTop(screen *ebiten.Image, card_back *ebiten.Image) {
+	card_back_op := &ebiten.DrawImageOptions{}
+	card_back_op.GeoM.Translate(float64(1280-15*75), float64(0))
+	card_back_op.GeoM.Scale(.25, .25)
+
+	for i := 0; i < 16; i++ {
+		screen.DrawImage(card_back, card_back_op)
+		card_back_op.GeoM.Translate(float64(75), float64(0))
+	}
+}
