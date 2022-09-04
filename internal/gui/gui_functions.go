5f3ecd61b75d81c89c4fcf0444fcbe7fc2f3810a
Jan Merhar <merharjan@gmail.com>
2022-09-05 01:29:27 +0200

    Add example main.go and DrawEntireDeckBottom() in gui/gui_functions.go

--- internal/gui/gui_functions.go
+++ internal/gui/gui_functions.go
@@ -12,3 +12,14 @@ func DrawEntireDeckTop(screen *ebiten.Image, card_back *ebiten.Image) {
 		card_back_op.GeoM.Translate(float64(75), float64(0))
 	}
 }
+
+func DrawEntireDeckBottom(screen *ebiten.Image, card_back *ebiten.Image) {
+	card_back_op := &ebiten.DrawImageOptions{}
+	card_back_op.GeoM.Translate(float64(1280-15*75), float64(2330))
+	card_back_op.GeoM.Scale(.25, .25)
+
+	for i := 0; i < 16; i++ {
+		screen.DrawImage(card_back, card_back_op)
+		card_back_op.GeoM.Translate(float64(75), float64(0))
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	_ "image/png"
+	"log"
 
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/janmerhar/tarock/internal/gui"
+)
+
+var (
+	background *ebiten.Image
+	card_back  *ebiten.Image
+	// horizontal_rectangle *image.RGBA
+)
+
+func init() {
+	background, _, _ = ebitenutil.NewImageFromFile("./img/table-top.png")
+	card_back, _, _ = ebitenutil.NewImageFromFile("./img/tarock22.png")
+	// horizontal_rectangle = ebiten.NewImageFromImage(image.Rect(100, 100, 100, 100))
+}
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.DrawImage(background, nil)
+
+	// card_back_op := &ebiten.DrawImageOptions{}
+	// card_back_op.GeoM.Translate(float64(0), float64(0))
+	// card_back_op.GeoM.Scale(.25, .25)
+	// screen.DrawImage(card_back, card_back_op)
+
+	// card_back_op.GeoM.Translate(float64(200), float64(0))
+	// screen.DrawImage(card_back, card_back_op)
+
+	// gui.DrawEntireDeck(screen, card_back)
+	gui.DrawEntireDeckTop(screen, card_back)
+	gui.DrawEntireDeckBottom(screen, card_back)
+	// card_back.Clear()
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 1280, 720
+}
+
+func main() {
+	ebiten.SetWindowSize(1280, 720)
+	ebiten.SetWindowTitle("Render an image")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
