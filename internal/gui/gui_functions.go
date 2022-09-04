7d0ee34500007b8837629371f76a0cab0ff30b60
Jan Merhar <merharjan@gmail.com>
2022-09-04 19:18:28 +0200

    Remove fyne.io code for migration to ebitengine

--- internal/gui/gui_functions.go
+++ internal/gui/gui_functions.go
@@ -1,19 +1,1 @@
 package gui
-
-import (
-	"fyne.io/fyne/v2"
-)
-
-func Load_image(img_path string) fyne.Resource {
-	icon, _ := fyne.LoadResourceFromPath(img_path)
-	return icon
-}
-
-func Create_game_window(a fyne.App) fyne.Window {
-	w := a.NewWindow("Solitaire")
-	w.SetPadded(false)
-	w.CenterOnScreen()
-	w.Resize(fyne.NewSize(1280, 720))
-
-	return w
-}
